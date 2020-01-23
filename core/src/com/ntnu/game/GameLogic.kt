6cab3c16c9567dda7d646fe433a834aac4dbd062
Bartha Bálint <bartha.balint@hotmail.com>
2020-01-23 20:47:22 +0100

    Helicopter collision

--- core/src/com/ntnu/game/GameLogic.kt
+++ core/src/com/ntnu/game/GameLogic.kt
@@ -16,6 +16,25 @@ class GameLogic(private val controller: Controller? = null) {
 
     fun update(dt: Float){
         for(h:Helicopter in helicopters){
+
+            for(h2:Helicopter in helicopters) {
+                if (h.position.x + HelicopterGame.HELICOPTER_WIDTH + h.movement.x > h2.position.x &&
+                        h.position.x + h.movement.x < h2.position.x + HelicopterGame.HELICOPTER_WIDTH &&
+                        h.position.y + HelicopterGame.HELICOPTER_HEIGHT > h2.position.y &&
+                        h.position.y < h2.position.y + HelicopterGame.HELICOPTER_HEIGHT) {
+                    h.switchX()
+                    h2.switchX()
+                }
+
+                if (h.position.x + HelicopterGame.HELICOPTER_WIDTH > h2.position.x &&
+                        h.position.x < h2.position.x + HelicopterGame.HELICOPTER_WIDTH &&
+                        h.position.y + HelicopterGame.HELICOPTER_HEIGHT + h.movement.y > h2.position.y &&
+                        h.position.y + h.movement.y < h2.position.y + HelicopterGame.HELICOPTER_HEIGHT) {
+                    h.switchY()
+                    h2.switchY()
+                }
+            }
+
             val predictedPos = Vector2(h.position.x + h.movement.x, h.position.y + h.movement.y)
 
             if(predictedPos.x < 0){
@@ -34,6 +53,7 @@ class GameLogic(private val controller: Controller? = null) {
                 predictedPos.y = upperWall - (predictedPos.y + HelicopterGame.HELICOPTER_HEIGHT - HelicopterGame.SCREEN_HEIGHT)
                 h.switchY()
             }
+
             if (controller != null){
                 h.movement = controller.getDelta()
                 h.position = predictedPos
@@ -49,5 +69,9 @@ class GameLogic(private val controller: Controller? = null) {
             h.render(sb)
         }
         controller?.render()
+    }
+
+    fun collideHelicopters(h1: Helicopter, h2: Helicopter){
+        //
     }
 }
--- core/src/com/ntnu/game/states/play/PlayState3.kt
+++ core/src/com/ntnu/game/states/play/PlayState3.kt
@@ -5,21 +5,17 @@ import com.badlogic.gdx.math.Vector2
 import com.ntnu.game.GameLogic
 import com.ntnu.game.sprites.Helicopter
 import com.ntnu.game.states.IState
+import kotlin.random.Random
 
 class PlayState3 : IState {
-
-    private val helicopter1: Helicopter = Helicopter(Vector2(2f, 3f), Vector2(5f, 20f))
-
-    private val helicopter2: Helicopter = Helicopter(Vector2(3f, -4f), Vector2(200f, 500f))
-
-    private val helicopter3: Helicopter = Helicopter(Vector2(-4f, 2f), Vector2(150f, 5f))
 
     private val logic: GameLogic = GameLogic()
 
     init {
-        logic.helicopters.add(helicopter1)
-        logic.helicopters.add(helicopter2)
-        logic.helicopters.add(helicopter3)
+        for (i in 1..5) {
+            logic.helicopters.add(Helicopter(Vector2(Random.nextFloat()*10f, Random.nextFloat() * 10f)))
+        }
+
     }
 
     override fun update(dt: Float) {
